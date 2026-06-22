c35dcdac71dfc52ee6684aa9832aa729ef171287
Rafael CELESTRE <rafael.celestre@gmail.com>
2026-06-22 16:32:39 +0200

    fix: setup.py dependencies
     Changes to be committed:
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ INSTALL_REQUIRES = (
                     'numba',
                     'oasys-barc4ro>=2024.11.13',
                     'barc4shadow>=2026.06.17',
-                    'barc4beams>=2026.06.17',
+                    'barc4beams>=2026.06.22',
                     "joblib",
                     # 'spectrum-builder>=1.0.5',     # to be added in the future, now install manually
                     # 'accelerator-toolbox==0.6.1', # in stand-by, gives problems in windows
