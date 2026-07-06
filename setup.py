323dc9538a2019e9f418faa32753f3a41db9d602
Manuel Sanchez del Rio <srio@esrf.eu>
2026-07-06 15:38:43 +0200

    cleaning laue crystal (moved to official wofry)

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ import sys
 from setuptools import find_packages, setup
 
 NAME = 'OASYS2-ESRF-EXTENSIONS'
-VERSION = '0.0.17'
+VERSION = '0.0.18'
 ISRELEASED = False
 
 DESCRIPTION = 'oasys2-esrf-extensions'
